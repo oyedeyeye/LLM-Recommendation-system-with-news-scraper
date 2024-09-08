833fe61ff0a10721dadb0259dd6ceabf3af2079d
Hezekiah Oyemade <oyedey@gmail.com>
2024-09-08 21:01:18 +0100

    redis pipeline

---
+++ scrapers/pipelines.py
@@ -0,0 +1,22 @@
+import json
+import redis
+from itemadapter import ItemAdapter
+
+
+class RedisWriterPipeline:
+    """
+    Redis writer operations
+    """
+    def __init__(self) -> None:
+        self.idx = 0
+        self.redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+    def process_item(self, item, spider):
+        self.redis_client.hset(f'item-{self.idx}', mapping={
+            'title': item['title'],
+            'url': item['url']
+        })
+        self.idx += 1
+
+    def get(self, key):
+        return self.redis_client.hgetall(key)
