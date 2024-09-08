026c5f987de5fe3309fc52ae538a9100b4905579
Hezekiah Oyemade <oyedey@gmail.com>
2024-09-08 21:39:06 +0100

    Prompter class

---
+++ large_models/prompters/prompter.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+
+class Prompter(ABC):
+    """
+    Base class for prompters. Prompters are responsible for generating decorated propmts
+    """
+    _system_description = None
+
+    def __init__(self, system_description):
+        pass
+
+    @abstractmethod
+    def generate_prompt(self, text):
+        """
+        Generate a decorated prompt
+        """
+        raise NotImplementedError()
